062ca874b5115bf76ea8fa2fb37c364e8c519690
Phuclmao <caophuc102005@gmail.com>
2024-12-13 00:54:03 +0700

    add sound

---
+++ src/audio/AudioPlayer.java
@@ -0,0 +1,123 @@
+package audio;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class AudioPlayer {
+
+    public static int MENU = 0;
+    public static int LEVEL_1 = 1;
+    public static int LEVEL_2 = 2;
+
+    public static int DIE = 0;
+    public static int JUMP = 1;
+    public static int DASH = 2;
+    public static int GAMEOVER = 3;
+    public static int LVL_COMPLETED = 4;
+
+    private Clip[] songs, effects;
+    private int currentSongId;
+    private float volume = 1f;
+
+    public AudioPlayer() {
+        loadSongs();
+        loadEffects();
+        playSong(MENU);
+    }
+
+    private void loadSongs() {
+        String[] names = { "menu", "level1", "level2" };
+        songs = new Clip[names.length];
+        for (int i = 0; i < songs.length; i++) {
+            songs[i] = getClip(names[i]);
+        }
+    }
+
+    private void loadEffects() {
+        String[] effectNames = { "die", "jump", "dash", "gameover", "lvlcompleted" };
+        effects = new Clip[effectNames.length];
+        for (int i = 0; i < effects.length; i++) {
+            effects[i] = getClip(effectNames[i]);
+        }
+        updateEffectsVolume();
+    }
+
+    private Clip getClip(String name) {
+        URL url = getClass().getResource("/audio/" + name + ".wav");
+        AudioInputStream audio;
+
+        try {
+            audio = AudioSystem.getAudioInputStream(url);
+            Clip c = AudioSystem.getClip();
+            c.open(audio);
+            return c;
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException c) {
+            c.printStackTrace();
+        }
+        return null;
+    }
+
+    private void updateSongVolume() {
+        FloatControl gainControl = (FloatControl) songs[currentSongId].getControl(FloatControl.Type.MASTER_GAIN);
+        float range = gainControl.getMaximum() - gainControl.getMinimum();
+        float gain = (range * volume) + gainControl.getMinimum();
+        gainControl.setValue(gain);
+    }
+
+    private void updateEffectsVolume() {
+        for (Clip c : effects) {
+            FloatControl gainControl = (FloatControl) songs[currentSongId].getControl(FloatControl.Type.MASTER_GAIN);
+            float range = gainControl.getMaximum() - gainControl.getMinimum();
+            float gain = (range * volume) + gainControl.getMinimum();
+            gainControl.setValue(gain);
+        }
+    }
+
+    public void stopSong() {
+        if (songs[currentSongId].isActive())
+            songs[currentSongId].stop();
+    }
+
+    public void setLevelSong(int lvlIndex) {
+        if (lvlIndex % 2 == 0) {
+            playSong(LEVEL_1);
+        } else {
+            playSong(LEVEL_2);
+        }
+    }
+
+    public void lvlCompleted() {
+        stopSong();
+        playSong(LVL_COMPLETED);
+    }
+
+    public void playEffect(int effect) {
+        effects[effect].setMicrosecondPosition(0);
+        effects[effect].start();
+    }
+
+    public void playSong(int song) {
+        stopSong();
+        currentSongId = song;
+        updateSongVolume();
+        songs[currentSongId].setMicrosecondPosition(0);
+        songs[currentSongId].loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void setVolume(float volume) {
+        this.volume = volume;
+        updateSongVolume();
+        updateEffectsVolume();
+    }
+
+    public float getVolume() {
+        return volume;
+    }
+}
